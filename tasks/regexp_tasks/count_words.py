27e7a62a2719d118552e6ab1281c9d0fdfd0041a
emys <emys@mail.ru>
2022-08-22 15:09:41 +0300

    count_words

--- tasks/regexp_tasks/count_words.py
+++ tasks/regexp_tasks/count_words.py
@@ -3,3 +3,13 @@
 с учетом, что в начале могут быть пробелы, в конце могут быть пробелы, между слов
 может встречаться больше одного пробела подряд.
 """
+import re
+
+
+def count_words():
+    text = input("Add your text here")
+    lst = re.findall("\S+", text)
+    print(len(lst))
+
+
+count_words()
